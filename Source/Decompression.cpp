4aa31720eb8a619678758521430ef208ea7d4a25
fnlnandy <andyfaniloniaina01@gmail.com>
2024-07-02 13:06:37 +0300

    Documented Decompression.hpp.

--- Source/Decompression.cpp
+++ Source/Decompression.cpp
@@ -1,14 +1,29 @@
 #include "Common.hpp"
 
+/**
+ * @param encodedData
+ *
+ * @brief Tries to deduce an encoding
+ * table from the given binary data.
+ *
+ * @warning This changes encodedData.
+ *
+ * @return The deduced encoding table.
+ * @returns Dict<char, uint>
+ */
 static Dict<char, uint> deduceEncodingTableFromContent(BinContainer &encodedData)
 {
     Dict<char, uint> encodingTable = {};
 
+    // We start with 1, since globally 0 is
+    // used as borth a separator and a header terminator,
+    // we also don't include 16 since we can only encode
+    // 15 characters (16 - 1).
     for (uint i = 1; i < 16; ++i)
     {
         uint8_t buffer = encodedData.pop8Bits();
 
-        if (buffer == 0) // Separator, also table terminator.
+        if (buffer == 0)
             break;
 
         encodingTable[static_cast<char>(buffer)] = i;
@@ -30,6 +45,7 @@ std::string decompressBinaryToText(BinContainer &encodedData)
         {
             uint8_t buffer = encodedData.pop8Bits();
 
+            // Separator encountered, switching the mode.
             if (buffer == 0)
             {
                 isEncodedMode = true;
@@ -43,12 +59,14 @@ std::string decompressBinaryToText(BinContainer &encodedData)
             uint8_t currChar = encodedData.pop4Bits();
             uint8_t nextChar = encodedData.peek4Bits();
 
+            // Separator encountered, switching the mode.
             if (currChar == 0 && prevChar == 0)
             {
                 isEncodedMode = false;
                 prevChar = -1;
                 continue;
             }
+            // Also separator encountered.
             else if (currChar == 0 && nextChar == 0)
             {
                 // Peek did NOT take the 4 bits, so we have to.
